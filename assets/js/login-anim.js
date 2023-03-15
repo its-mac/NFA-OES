8691fefac716f8c9f7a595a128f504100893f9d2
MAC <bc220415372mab@vu.edu.pk>
2023-03-15 17:15:21 +0500

    Login & Register Page

--- assets/js/login-anim.js
+++ assets/js/login-anim.js
@@ -1,31 +1,9 @@
-let register = document.querySelector('.register');
-let login = document.querySelector('.login');
-
-let zIndex = 1;
-
-login.addEventListener("click", (e) => {
-    register.style.transform = "translateY(-300px)"
-    register.addEventListener("transitionend", (e) => {
-        register.style.zIndex = zIndex++
-        register.addEventListener("transitionend", (e) => {
-            register.style.width = "500px"
-            register.style.transform = "translateY(0px)"
-            login.style.width = "300px"
-            login.style.transform = "translateY(-100px)"
-        }, { once: true })
-    }, { once: true })
+let inforBtns = document.querySelectorAll(".info-item .btn");
+inforBtns.forEach((item) => {
+    item.addEventListener("click", () => {
+        document.querySelector(".container").classList.toggle("log-in");
+    });
 })
-
-register.addEventListener("click", (e) => {
-    login.style.transform = "translateY(-300px)"
-    login.addEventListener("transitionend", (e) => {
-        login.style.zIndex = zIndex++
-        login.addEventListener("transitionend", (e) => {
-            login.style.width = "500px"
-            login.style.transform = "translateY(0px)"
-            register.style.width = "300px"
-            register.style.transform = "translateY(-100px)"
-        }, { once: true })
-    }, { once: true })
-})
-
+document.querySelector(".container-form .btn").addEventListener("click", () => {
+    document.querySelector(".container").classList.add("active");
+});
