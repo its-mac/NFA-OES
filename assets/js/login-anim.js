6d246a9692f3bf267623cf7a485b8ca66d5d4d47
MAC <bc220415372mab@vu.edu.pk>
2023-03-17 14:17:06 +0500

    Loader styling

--- assets/js/login-anim.js
+++ assets/js/login-anim.js
@@ -1,9 +1,7 @@
-let inforBtns = document.querySelectorAll(".info-item .btn");
-inforBtns.forEach((item) => {
+let infoBtns = document.querySelectorAll(".info-item .btn");
+let formBtns = document.querySelector(".container-form .btn");
+infoBtns.forEach((item) => {
     item.addEventListener("click", () => {
         document.querySelector(".container").classList.toggle("log-in");
     });
 })
-document.querySelector(".container-form .btn").addEventListener("click", () => {
-    document.querySelector(".container").classList.add("active");
-});
