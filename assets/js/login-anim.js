16944c1cc4aa0e52d8fbefbdeee6b15892438fa2
MAC <bc220415372mab@vu.edu.pk>
2023-03-21 23:35:36 +0500

    Form Buttons of no need

--- assets/js/login-anim.js
+++ assets/js/login-anim.js
@@ -1,5 +1,4 @@
 let infoBtns = document.querySelectorAll(".info-item .btn");
-let formBtns = document.querySelector(".container-form .btn");
 infoBtns.forEach((item) => {
     item.addEventListener("click", () => {
         document.querySelector(".container").classList.toggle("log-in");
